7eef51679e329301ca875abfe77967b9e0c3fff8
abdcantcode <abdullah72ftw@gmail.com>
2025-12-19 02:17:32 +0530

    gsap use karna aaya thoda

---
+++ Day 2-GSAP/script.js
@@ -0,0 +1,5 @@
+gsap.to('.box',{
+  x:500,
+  duration:5,
+  delay:1
+})
---
+++ Day 2/script.js
@@ -0,0 +1,7 @@
+function hero(){
+  console.log("Hello");
+  return 0;
+}
+var a=hero();//Isse yeh function call hojara aur a mein 0 aajata
+console.log(hero());
+//Agar koi function ku console.log karey toh uske andar ka console log aata aur agar uney kuch return nai karra toh undefined aata uney return karra toh woh value aati warna nai aati kuch bhi 

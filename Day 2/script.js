9d906c16ab23614bb1966a634898706b2ed037b9
abdcantcode <abdullah72ftw@gmail.com>
2025-12-19 02:38:56 +0530

    create element aur append child

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,7 +1,13 @@
-function hero(){
-  console.log("Hello");
-  return 0;
-}
-var a=hero();//Isse yeh function call hojara aur a mein 0 aajata
-console.log(hero());
+// function hero(){
+//   console.log("Hello");
+//   return 0;
+// }
+// var a=hero();//Isse yeh function call hojara aur a mein 0 aajata
+// console.log(hero());
 //Agar koi function ku console.log karey toh uske andar ka console log aata aur agar uney kuch return nai karra toh undefined aata uney return karra toh woh value aati warna nai aati kuch bhi 
+
+//React aur React Dom alag alag cheezan hai react ku dom se connect karne use hota react dom 
+// let h1=document.createElement('h1');
+// h1.innerHTML='Kya bolte ho ladlo';
+// let body=document.querySelector('body');
+// body.appendChild(h1);

d8d8698183f923d393b5286e9f7ea724beef42fd
abdcantcode <abdullah72ftw@gmail.com>
2025-12-19 01:12:29 +0530

    default export and import

---
+++ Day 1/app.js
@@ -0,0 +1,9 @@
+
+//React mein do type ke exports rehte
+//default export aur normal export 
+//Per file khali ek default export kar sakte
+
+let a=10;
+export default a;
+
+//abhi mai aur ek default export nai kar saktoun 
---
+++ Day 1/script.js
@@ -0,0 +1,10 @@
+import defexportvariable from './app.js'
+//yeh default import statement hai re mama
+//Multiple default imports reh sakte ek file mein lekin ek file ku ek ich default export reh sakta mama 
+
+//import karru so file mein mai kubh bhi naam se le saktu default export vaale variable ku
+console.log(defexportvariable);
+
+
+
+

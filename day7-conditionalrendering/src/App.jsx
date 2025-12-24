28d6c27a2e0f7cf00614f342b2bb0e0d5ef6c27a
abdcantcode <abdullah72ftw@gmail.com>
2025-12-24 07:22:58 +0530

    buttons

---
+++ day7-conditionalrendering/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Men from './components/Men'
+const App = () => {
+  return (
+    <div>
+      <Men/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ day7-conditionalrendering/src/components/Women.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Women = () => {
+  return (
+    <div className='bg-pink-400 border-2 border-pink-600 m-2 h-10 w-40 text-nowrap flex items-center justify-center'>
+      Women's Washroom
+    </div>
+  )
+}
+
+export default Women

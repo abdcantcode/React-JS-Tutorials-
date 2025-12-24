042158c3610274ad8e210669eec2f92833f514c1
abdcantcode <abdullah72ftw@gmail.com>
2025-12-24 07:34:26 +0530

    my conditional rendering approach

--- day7-conditionalrendering/src/App.jsx
+++ day7-conditionalrendering/src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import Men from './components/Men'
+import Women from './components/Women';
 const App = () => {
+  const users = [
+    {
+      name: "Rahul",
+      email: "rahul@gmail.com",
+      gender: "male",
+      color: "blue"
+    },
+    {
+      name: "Amit",
+      email: "amit@gmail.com",
+      gender: "male",
+      color: "green"
+    },
+    {
+      name: "Priya",
+      email: "priya@gmail.com",
+      gender: "female",
+      color: "pink"
+    },
+    {
+      name: "Neha",
+      email: "neha@gmail.com",
+      gender: "female",
+      color: "purple"
+    }
+  ];
+
   return (
     <div>
-      <Men/>
+      {users.map(function(elem){
+        let card= elem.gender==='male'?<Men/>:<Women/>
+        return card;
+      })}
     </div>
   )
 }

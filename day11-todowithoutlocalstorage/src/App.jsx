451a67ce333d84bf8b7e4d78c08c55e008894c13
abdcantcode <abdullah72ftw@gmail.com>
2025-12-29 13:34:13 +0530

    CARD DESIGN DONE

--- day11-todowithoutlocalstorage/src/App.jsx
+++ day11-todowithoutlocalstorage/src/App.jsx
@@ -6,10 +6,19 @@ const App = () => {
   const [profession,setProfession]=useState('');
   const [url,setUrl]=useState('');
   const [desc,setDesc]=useState('');
+  let [details,setDetails]=useState([]);
 
   function submitHandler(e){
-    e.preventDefault();
-    setName('');
+    e.preventDefault(); //Preventing default form behaviour
+
+    let copyofdetails=[...details];
+    copyofdetails.push({name,profession,url,desc})
+    setDetails(copyofdetails);
+    console.log(copyofdetails);
+
+
+
+    setName('');  //After submission making those input tags go blank
     setProfession('');
     setDesc('');
     setUrl('');
@@ -18,11 +27,11 @@ const App = () => {
 
 
   return (
-    <div>
-      <form onSubmit={(e)=>{
+    <div className='w-full p-5 bg-black h-full flex flex-col'>
+      <form className='flex gap-2 flex-wrap w-fit  w-full' onSubmit={(e)=>{
         submitHandler(e);
       }}>
-        <input type="text" 
+        <input className='border-2 w-[49%] border-white text-white rounded-lg p-2'  type="text" 
         onChange={(e)=>{
           setName(e.target.value);
         }}
@@ -30,7 +39,7 @@ const App = () => {
         placeholder='Enter your name'
         required/>
 
-        <input type="text" 
+        <input className='border-2 border-white text-white w-[49%] rounded-lg p-2' type="text" 
         onChange={(e)=>{
           setProfession(e.target.value);
         }}
@@ -38,7 +47,7 @@ const App = () => {
         placeholder='Enter your profession'
         required/>
 
-        <input type="text" 
+        <input className='border-2 border-white text-white w-[49%] rounded-lg p-2' type="text" 
         onChange={(e)=>{
           setUrl(e.target.value);
         }}
@@ -46,7 +55,7 @@ const App = () => {
         placeholder='Enter your Image URL'
         required/>
 
-        <input type="text" 
+        <input className='border-2 border-white text-white w-[49%] rounded-lg p-2' type="text" 
         onChange={(e)=>{
           setDesc(e.target.value);
         }}
@@ -54,8 +63,28 @@ const App = () => {
         placeholder='Enter your description'
         required/>
 
-        <button>Submit</button>
+        <button className='bg-emerald-500 px-16 py-2 rounded-lg font-bold active:scale-95 m-auto'>Submit</button>
       </form>
+        <div className='flex gap-2 my-3'>
+
+        
+
+      <div className='flex flex-col gap-3 h-fit w-[22%] bg-white rounded-lg items-center py-4'>
+        <img className='object-cover object-center rounded-[50%] w-[80px] h-[80px] ' src="https://images.pexels.com/photos/15260732/pexels-photo-15260732.jpeg" alt="" />
+        <h3 className='text-[18px] font-bold'>Abdullah Aajaz</h3>
+        <h4 className='text-blue-400 font-semibold'>Developer</h4>
+        <p className='text-sm text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi, magni?
+        </p>
+        <button className='bg-red-500 px-4 text-white active:scale-90 font-bold py-2 rounded-lg'>Remove</button>
+      </div>
+      
+
+
+
+        </div>
+      
+      
+
     </div>
   )
 }

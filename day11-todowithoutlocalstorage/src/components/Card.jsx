d7ed06130f10cfd648630e58ec43af77771a1885
abdcantcode <abdullah72ftw@gmail.com>
2025-12-30 10:36:11 +0530

    Changes

--- day11-todowithoutlocalstorage/src/components/Card.jsx
+++ day11-todowithoutlocalstorage/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   
   return (
     <div className='flex flex-col gap-3 h-fit w-[22%] bg-white rounded-lg items-center py-4'>
-        <img className='object-cover object-center rounded-[50%] w-[80px] h-[80px] ' src={props.info.url} alt="" />
+        <img className='object-cover object-center rounded-[50%] w-20 h-20 ' src={props.info.url} alt="" />
         <h3 className='text-[18px] font-bold'>{props.info.name}</h3>
         <h4 className='text-blue-400 font-semibold'>{props.info.profession}</h4>
         <p className='text-sm text-center'>{props.info.desc}

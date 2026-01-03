22690646723602cba5ce814ef730717327e6a9e3
abdcantcode <abdullah72ftw@gmail.com>
2026-01-03 23:54:13 +0530

    routes and route

--- day15-routing/src/App.jsx
+++ day15-routing/src/App.jsx
@@ -1,9 +1,11 @@
-
+import { Routes,Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div className='underline text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-      <h1>Home Page</h1>
+    <div>
+      <Routes>
+        <Route to='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ day15-routing/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='underline text-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'> 
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home

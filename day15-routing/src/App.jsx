26ef8de4b75690ab6695f5ef3281d3293f40ad87
abdcantcode <abdullah72ftw@gmail.com>
2026-01-03 23:59:20 +0530

    imports

--- day15-routing/src/App.jsx
+++ day15-routing/src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes,Route } from "react-router-dom"
-import Contact from "./pages/Contact"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

bf18f5c1d76b7f4aed2a90aca206649eeca2ab78
abdcantcode <abdullah72ftw@gmail.com>
2026-01-08 18:55:44 +0530

    changes prior

--- day21-contextapi-2/src/App.jsx
+++ day21-contextapi-2/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Section from '../components/Section'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Section from './components/Section'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div className='app'>
--- day21-contextapi-2/src/components/Section.jsx
+++ day21-contextapi-2/src/components/Section.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Section1 from './Section1'
+import Section2 from './Section2'
 
 const Section = () => {
   return (
     <div className='section'>
-      <h1>Section</h1>
+      <Section1/>
+      <Section2/>
     </div>
   )
 }

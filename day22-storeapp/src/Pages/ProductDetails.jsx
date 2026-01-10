587fb38505e51ea0765853d2444f8a2e6dbcba21
abdcantcode <abdullah72ftw@gmail.com>
2026-01-11 00:29:45 +0530

    md bs

--- day22-storeapp/src/Pages/ProductDetails.jsx
+++ day22-storeapp/src/Pages/ProductDetails.jsx
@@ -4,16 +4,13 @@ import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
   let data=useContext(ProductContextProvider);
-  let params=useParams();
+  let id=useParams();
   console.log(data);
-  console.log(params);
+  console.log(id);
   console.log(data[0])
   return (
     <div>
-      <h1>{data.map(function(elem,idx){
-        if((idx+1)===params)return <h1 key={idx}>{elem.title}</h1>
-        else return null
-      })}</h1>
+      <h1></h1>
       
     </div>
   )
